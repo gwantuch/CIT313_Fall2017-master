7de868810d87306ab60338d0dcfa828bdf7e63ad
gwantuch <31348651+gwantuch@users.noreply.github.com>
2017-09-09 12:03:23 -0400

    a1
    a1 debugger

---
+++ CIT313_Fall2017-master/CIT313_Fall2017-master/Debug-Exercise-master/webconfig.php
@@ -0,0 +1,5 @@
+<?php
+
+define("ABSOLUTE_PATH", "/home/gwantuch/htdocs/CIT313/FALL2017/a1/Debug-Exercise-master");
+define("URL_ROOT", "http://corsair.cs.iupui.edu:22021/CIT313/FALL2017/a1/Debug-Exercise-master/");
+?>
